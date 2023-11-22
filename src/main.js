d86b22d5d9ff4a62399e19b5ceab6579cfe2abe4
Bernardo Bressan <bressan.mattos@estudante.ufjf.br>
2023-11-22 20:49:55 -0300

    Completa todos os testes do Vitest

--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
-import { getMaria, getJoao } from "./joaoEMaria.js";
+import { getMaria, getJoao, deJoaoParaMaria, setMaria, setJoao } from "./joaoEMaria.js";
+
+setMaria(4);
+setJoao(2);
 
 console.log("MAIN");
 let maria = getMaria();
 let joao = getJoao();
+console.log(maria, joao);
+
+deJoaoParaMaria();
+
+maria = getMaria();
+joao = getJoao();
 console.log(maria, joao);
